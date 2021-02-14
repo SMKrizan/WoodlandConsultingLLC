c9343e48aff33c31b353c61fd4dbe138e266da03
Mona Lisa <joannagrosshans2@gmail.com>
2021-02-14 10:09:08 -0600

    resolvers and typeDefs updated

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,7 +5,33 @@ const typeDefs = gql`
     type Query {
         WoodlandConsulting: String
     }
-    
+    type Category {
+        _id: ID
+        name: String
+    }
+
+    type Project {
+        _id: ID
+        name: String
+        desctiption: String
+        image: String
+        date: String
+        location: String
+        category: Category
+
+    }
+
+    type Admin {
+        _id: ID
+        firstName: String
+        lastName: String
+        email: String
+    }
+
+    type Auth {
+        token: ID
+        user: Admin
+    }
 `;
 
 module.exports = typeDefs;
