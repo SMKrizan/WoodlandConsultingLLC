96620adf78aec8f6fe045224b5a3e1f2f36552c1
MarynaPR <pryadkamaryna@gmail.com>
2021-02-14 21:45:54 -0600

    demo map displaying on page

--- server/models/Map.js
+++ server/models/Map.js
@@ -1,1 +1,41 @@
 //  project name, description, city, state, latitude, longtitude, category, date
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const portfolioSchema = new Schema({
+
+    project: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String
+    },
+    city: {
+        type: String
+    },
+    state: {
+        type: String
+    },
+    date: {
+        type: String
+    },
+    latitude: {
+        type: integer 
+    },
+    longtitude: {
+        type: Int
+    },
+    // location: {
+    //     type: String
+    // }
+    category: {
+        type: String
+    }
+});
+
+const Map = mongoose.model('Map');
+
+module.exports = Map;
