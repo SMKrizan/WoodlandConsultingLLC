fbd6ee99c5c196c63b83db672f68a9f35c5a7a88
MarynaPR <pryadkamaryna@gmail.com>
2021-02-14 18:51:58 -0600

    map options review

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import L from 'leaflet';
+// import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+// import 'leaflet/dist/leaflet.css';
+// // import * as serviceWorker from './serviceWorker';
 import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +20,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
