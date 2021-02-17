d4b13f39f8ca20c5a963eca55a39c75e9fbb0694
Mona Lisa <joannagrosshans2@gmail.com>
2021-02-17 10:17:08 -0600

    typeDefs updated

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -55,10 +55,35 @@ const typeDefs = gql`
         message: String
         purpose: String
     }
+    input portfolioProject {
+        projectName: String
+        description: String
+        image: String!
+        category: Category
+    }
+    input client {
+        company: String
+        description: String
+        location: Point
+        WC: Boolean
+    }
+    input map {
+        image: String
+        WC: Boolean
+        location: Point
+        category: Category
+    }
     
     type Mutation {
         login(email: String!, password: String!): Auth
+        updateAdmin(firstName: String, lastName: String, email: String, cityState: String): Admin
         addTestimonial(name: String, company: String, message: String): Testimonial
+        addToPortfolio(projectData: projectProject!): Project
+        clientList(clientData: client!): Project
+        markerProject(mapData: map!): Project
+        updateTestimonial(name: String, company: String, message: String): Testimonial
+        removeTestimonial(_id: ID!): Testimonial
+        removeMessage(_id: ID!): UserForm
     }
 `;
 
@@ -67,12 +92,6 @@ module.exports = typeDefs;
 // updateTestimonial(firstName: String, lastName: String, company: String, message: String): Testimonial
 // removeTestimonial(_id: ID!): Testimonial
 // removeMessage
-
-// location: Point
-// }
-// type Porfolio {
-//     _id: ID
-//     name: String
 
 // make image required in mutation for portfolio
 // null for not image
