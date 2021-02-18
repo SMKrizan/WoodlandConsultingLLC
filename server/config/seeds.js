7f37fac1fa39c4852dad8056c11e517b56e41ff8
Mona Lisa <joannagrosshans2@gmail.com>
2021-02-18 09:55:50 -0600

    seeds file category, admin, messages, testimonials added

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,60 +1,58 @@
 const db = require('./connection');
-const { Category, Project, Map, Admin } = require('../models');
+const { Category, Project, Map, Admin, Testimonial } = require('../models');
 
 db.once('open', async () => {
     await Category.deleteMany();
 
     const categories = await Category.insertMany([
-        { name: 'Libraries/Schools' },
-        { name: 'Healthcare/Hospice' },
-        { name: 'Commercial/Office' },
-        { name: 'Retail/Restaurant' },
-        { name: 'Industrial/Transport Hubs' }
+        { categoryName: 'Libraries/Schools' },
+        { categoryName: 'Healthcare/Hospice' },
+        { categoryName: 'Commercial/Office' },
+        { cateogryName: 'Retail/Restaurant' },
+        { categoryName: 'Industrial/Transport Hubs' }
     ]);
 
     console.log('CATEGORIES SEEDED');
 
+    await Location.deleteMany();
+
+    const locations = await Location.insertMany([
+        {
+            latitude: '',
+            longitude: ''
+        }
+    ]);
+
+    console.log('locations seeded')
+
     await Admin.deleteMany();
 
     await Admin.create({
-        firstName: 'Jessica A.',
-        lastName: 'Walther',
-        email: 'jessica@gmail.com',
-        password: 'password12345'
+        adminName: 'Jessica A. Walther',
+        adminEmail: 'jessica@gmail.com',
+        password: 'password12345',
+        address: 'Slinger, WI'
     });
 
     console.log('admin seeded');
 
-    // await Project.deleteMany();
+    await Project.deleteMany();
 
-    // const projects = await Project.insertMany([
-    //     {
-    //         name: '',
-    //         description: '',
-    //         image: '',
-    //         date: '',
-    //         location: '',
-    //         category: 'categories[]._id'
-    //     }
-    // ])
-
-    // console.log('projects seeded');
-
-    await Map.deleteMany();
-
-    await Map.create([
+    await Project.create([
         {
             projectName: '',
             description: '',
-            category: 'categories[]._id',
+            image: '',
+            projectDate: '',
             cityState: '',
-            longtitude: '',
-            latitude: '',
-            projectDate: ''
+            location: 'locations[]._id',
+            category: 'categories[]._id',
+            company: '',
+            WC: ''
         }
     ]);
 
-    console.log('map seeded');
+    console.log('projects seeded');
 
     process.exit();
 })
@@ -135,15 +133,60 @@ db.once('open', async () => {
 
     // console.log('Projects seeded');
 
-    // await User.deleteMany();
-    // await User.create({
-    //     firstName: 'username',
-    //     lastName: 'lastname',
-    //     email: 'test@testmail.com',
-    //     password: 'password12345',
-    //     projects: [projects[0]._id, projects[0]._id, projects[1]._id]
-    // });
-    // console.log('users seeded');
+    await Message.deleteMany();
 
-//     process.exit();
+    const messages = await Message.insertMany([
+        {
+            userName: 'Bill Wonder',
+            company: '',
+            userEmail: 'bill@gmail.com',
+            purpose: 'Ask a question',
+            userMessage: 'What is your availability for the next few months?'
+        },
+        {
+            userName: 'Ashley Bunt',
+            company: 'Pottery Barn',
+            userEmail: 'ashley@gmail.com',
+            purpose: 'Leave a comment',
+            userMessage: 'Thank you for your work.'
+        },
+        {
+            userName: 'Kelly Woodman',
+            company: 'Woodman lighting',
+            userEmail: 'kelly@gmail.com',
+            purpose: 'Request a quote',
+            userMessage: 'I am looking for a lighitng plan and quote for a remodel of the home of a client.'
+        },
+        {
+            userName: 'Sully Dogman',
+            company: 'Bright for you',
+            userEmail: 'sully@gmail.com',
+            purpose: 'Provide a testimonial',
+            userMessage: 'Jessica is very professional, communicative, and open to new ideas for design. She is simply the best to work with!'
+        }
+    ]);
+
+    console.log('messages seeded')
+
+    await Testimonial.deleteMany();
+
+    const testimonials = await Testimonial.insertMany([
+        {
+            tstName: 'Joe Light',
+            company: 'Lighting Design',
+            tstMessage: 'Woodland Consulting is the best in the business!'
+        },
+        {
+            tstName: 'Jill Bulb',
+            company: 'Lighting for all',
+            tstMessage: 'Jessica at Woodland Consulting is a great business partner and really dedicates her time to each job.'
+        },
+        {
+            tstName: 'Jack Bright',
+            company: 'Lighting Bright',
+            tstMessage: 'I would recommend Woodland Consulting for any lighting project!'
+        }
+    ]);
+
+    console.log('testimonials seeded')
 // });
