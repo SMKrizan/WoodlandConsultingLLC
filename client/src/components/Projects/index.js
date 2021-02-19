ae6852e43b972579fbe16580bdde37f6921ab50a
MarynaPR <pryadkamaryna@gmail.com>
2021-02-19 14:30:44 -0600

    googlemap folder to separate projects from map

---
+++ client/src/components/Projects/index.js
@@ -0,0 +1,33 @@
+
+// import React, { useEffect } from "react";
+// import { useStoreContext } from "../../utils/GlobalState";
+// import { GET_PROJECTS } from "../../utils/actions";
+
+// import { useQuery } from '@apollo/react-hooks';
+// import { idbPromise } from "../../utils/helpers";
+// import { useQuery } from '@apollo/react-hooks';
+// import { idbPromise } from "../../utils/helpers";
+
+
+// function Projects() {
+//     //state, effect
+//     return (
+//         <div className="">
+//             <h2>Projects:</h2>
+//             {state.project.length ? (
+//                 <div className="">
+//                     {filterProjects().map(project => (
+//                         <Project
+//                             key={project._id}
+//                             _id={project._id}
+//                             name={project.projectNname}
+//                         />
+//                     ))}
+//                 </div>
+//             ) : (
+//                     <h3>hello</h3>
+//                 )}
+//         </div>
+//     );
+
+//     export default Projects;
---
+++ client/src/pages/GoogleMap/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Projects from "../components/Projects";
+
+
+const GoogleMap = () => {
+    return (
+        <div >
+            <Projects />
+        </div>
+    );
+};
+
+export default GoogleMap;
