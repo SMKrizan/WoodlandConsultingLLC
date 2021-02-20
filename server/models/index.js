f42c59b4f8a214e3d0b3ee19374936730174d515
SMKrizan <smkrizan@gmail.com>
2021-02-19 19:22:37 -0600

    reversion to earlier

--- server/models/index.js
+++
@@ -1,9 +0,0 @@
-const Category = require('./Category');
-const Owner = require('./Owner');
-const Testimonial = require('./Testimonial');
-const Message = require('./Message');
-const Project = require('./Project')
-
-
-
-module.exports = { Category, Project, Owner, Testimonial, Message };
