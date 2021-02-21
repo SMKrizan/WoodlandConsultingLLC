3eb3c89e85fefebc6ef5edd88c3305fe54273b32
Cat <CGraham@CatherinesMBP2.attlocal.net>
2021-02-20 20:33:34 -0600

    added in map code and switched out map package

--- client/src/components/Gallery/index.js
+++ client/src/components/Gallery/index.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { useQuery} from '@apollo/react-hooks';
+
+
 import image1 from '../../assets/images/portfolio/Colliers_1.JPG';
+import { GET_PROJECTS } from '../../utils/queries';
 
 
 
 function Gallery() {
+    const { loading, data} = useQuery(GET_PROJECTS);
+    const projectData = data?.projects ;
+    console.log(projectData)
+    if (loading) {
+      return <div>Loading...</div>;
+    }
+    if (!projectData) {
+        console.log("no testimonials pulled....")
+        return <h2>LOADING...</h2>;
+      }
+
+
     return (
         <div className="gallery pad">
             <div className="project">
