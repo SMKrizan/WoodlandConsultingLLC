0598e57b3fecb076be6f5d5cd108b6f3a434daf6
MarynaPR <pryadkamaryna@gmail.com>
2021-02-22 21:24:00 -0600

    projec added

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -25,13 +25,16 @@ const resolvers = {
       console.log('Owner: ', owner)
       return owner
     },
+    projects: async () => {
+      return await Project.find().populate('category');
+    },
     projectsByCategory: async (parent, { category }) => {
       const params = {};
       if (category) {
         params.categoryName = category;
       }
 
-      const project  = Project.find({ category });
+      const project = Project.find({ category });
       return project
     },
     projectById: async (parent, { _id }) => {
@@ -67,7 +70,7 @@ const resolvers = {
     updateOwner: async (parent, args, context) => {
       if (context.owner) {
         console.log('owner:', context.owner._id, args)
-        const owner = await Owner.findByIdAndUpdate({_id: context.owner._id}, args, {
+        const owner = await Owner.findByIdAndUpdate({ _id: context.owner._id }, args, {
           new: true,
         });
         console.log('owner: ', owner)
@@ -92,31 +95,31 @@ const resolvers = {
           {
             new: true,
           }
-          
+
         );
         console.log("======================", updatedTestimonial)
         return updatedTestimonial;
-        
+
       }
-      
+
       throw new AuthenticationError("You must be logged in to perform this action.");
     },
-    removeTestimonial: async (parent, { _id }, context ) => {
+    removeTestimonial: async (parent, { _id }, context) => {
       if (context.owner) {
-      const updatedTestimonials = await Testimonial.findByIdAndDelete(
-        _id,
-      );
-      return updatedTestimonials;
+        const updatedTestimonials = await Testimonial.findByIdAndDelete(
+          _id,
+        );
+        return updatedTestimonials;
       }
       throw new AuthenticationError("You must be logged in to perform this action.")
     },
-    addMessage: async ( parent, args ) => {
-        const message = await Message.create({
-          ...args
-        });
-        return message ;
+    addMessage: async (parent, args) => {
+      const message = await Message.create({
+        ...args
+      });
+      return message;
     },
-    removeMessage: async ( parent, { _id }, context ) => {
+    removeMessage: async (parent, { _id }, context) => {
       if (context.owner) {
         const updatedMessageList = await Message.findByIdAndDelete(_id)
         return updatedMessageList
