5c349556c11f1d4ef3310f27f8ed9187da1ae6b6
SMKrizan <smkrizan@gmail.com>
2021-02-24 12:43:20 -0600

    branch reverted and contact page updated getting graphqlerror

--- client/src/pages/AdminPage/index.js
+++ client/src/pages/AdminPage/index.js
@@ -1,4 +1,3 @@
-// MVP: page holds admin profile data, data collected from user contact form, and form for entering testimonial info
 // Nice to have: location to enter client list data, location to add photos and photo data
 
 import React, { useState } from "react";
