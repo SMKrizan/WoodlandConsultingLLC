9facb01d9084638d84f39c0636ba8741bd91b032
SMKrizan <smkrizan@gmail.com>
2021-02-27 14:44:07 -0600

    ownerInfo data saving to idb

--- client/src/components/OwnerInfo/index.js
+++ client/src/components/OwnerInfo/index.js
@@ -1,18 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useQuery, useMutation } from '@apollo/react-hooks';
 import { GET_OWNER } from '../../utils/queries';
 import { UPDATE_OWNER } from '../../utils/mutations';
+import { UPDATE_OWNER_INFO } from '../../utils/actions';
 import { Modal } from 'react-responsive-modal';
-import 'react-responsive-modal/styles.css';
-const ManageOwnerInfo = (props) => {
+import { idbPromise } from '../../utils/helpers';
+import { useStoreContext } from "../../utils/GlobalState";
 
+import 'react-responsive-modal/styles.css';
+
+const ManageOwnerInfo = (props) => {
+    // hook establishes state variable and dispatches fn to update state
+    const [state, dispatch] = useStoreContext();
     const { loading, data } = useQuery(GET_OWNER);
     const ownerData = data?.owner || [];
-    console.log('ownerData: ', data?.owner);
+    // console.log('ownerData: ', data?.owner);
     const [updateOwner, { error }] = useMutation(UPDATE_OWNER);
+
     const handleFormSubmit = async (event) => {
         event.preventDefault();
-        console.log("LOG", newOwnerInfo);
+        // console.log("LOG", newOwnerInfo);
 
         const mutationResponse = await updateOwner({
             variables: {
@@ -22,7 +29,31 @@ const ManageOwnerInfo = (props) => {
                 address: newOwnerInfo.address
             },
         });
+        console.log('mutationResponse.data.updateOwner: ', mutationResponse.data.updateOwner)
+        
+            dispatch({
+                type: UPDATE_OWNER_INFO,
+                ownerInfo: mutationResponse.data
+            });
+            console.log('newOwnerInfo: ', newOwnerInfo)
+            idbPromise('owner', 'get', { newOwnerInfo })
+    
     };
+
+
+    // useEffect(() => {
+    //     // if there's data to be stored
+    //     if (ownerData) {
+    //       // let's store it in the global state object
+    //       dispatch({
+    //         type: GET_OWNER,
+    //         ownerInfo: data.ownerInfo,
+    //     });
+    //     // but let's also take each product and save it to IndexedDB using the helper function 
+    //     idbPromise('owners', 'put', owner);
+    //     }
+    //   }, [data, loading, dispatch]);
+    // }
 
     const [newOwnerInfo, setNewOwnerInfo] = useState({ ownerName: '', ownerEmail: '', address: '' });
     const handleClick = (data) => {
@@ -37,7 +68,7 @@ const ManageOwnerInfo = (props) => {
             ...newOwnerInfo,
             [name]: value,
         });
-    }
+    };
     const [open, setOpen] = React.useState(false);
     // const onOpenModal = () => setOpen(true);
     const onCloseModal = () => setOpen(false);
@@ -46,7 +77,7 @@ const ManageOwnerInfo = (props) => {
     }
     if (!ownerData) {
         return <h2>Something went wrong.</h2>;
-    }
+    };
 
     return (
         <>
@@ -95,6 +126,7 @@ const ManageOwnerInfo = (props) => {
             </Modal>
         </>
     );
+
 };
 
 export default ManageOwnerInfo;
