5b98a1647b026ac61cb74e31311c63562950a9f9
MarynaPR <pryadkamaryna@gmail.com>
2021-02-26 20:24:21 -0600

    router started

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,6 @@
 import React, { useState } from 'react';
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
-import { BrowserRouter as Router } from 'react-router-dom';
-
-// import {useSpring, animated} from 'react-spring'
-
 import './App.css';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -15,10 +11,11 @@ import AdminPage from './pages/AdminPage';
 import { StoreProvider } from './utils/GlobalState';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import AdminAccess from './components/AdminAccess';
 import 'react-responsive-modal/styles.css';
-// import { Fade } from 'reactstrap';
-
-//const cache = new InMemoryCache();
+import { ProtectedRoute } from './components/Protected';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import AuthService from "./utils/auth";
 
 const client = new ApolloClient({
   request: operation => {
@@ -33,45 +30,48 @@ const client = new ApolloClient({
   uri: '/graphql'
 });
 
-
 function App() {
 
   const [currentPage, handlePageChange] = useState('Home');
+  const [isAuthenticated, setAuthenticated] = useState(AuthService.loggedIn())
 
-  //const propsMove = useSpring({opacity: 1, from: {opacity: 0}});
+  // const renderPage = () => {
 
-  const renderPage = () => {
-
-    switch (currentPage) {
-      case 'Home':
-        return <Home handlePageChange={handlePageChange} />;
-      case 'About':
-        return <About />;
-      case 'Map':
-        return <Map />;
-      case 'Portfolio':
-        return <Portfolio />;
-      case 'Contact':
-        return <Contact />;
-      case 'AdminPage':
-        return <AdminPage />
-      default:
-        return <Home />;
-    }
-  };
+  //   switch (currentPage) {
+  //     case 'Home':
+  //       return <Home handlePageChange={handlePageChange} />;
+  //     case 'About':
+  //       return <About />;
+  //     case 'Map':
+  //       return <Map />;
+  //     case 'Portfolio':
+  //       return <Portfolio />;
+  //     case 'Contact':
+  //       return <Contact />;
+  //     case 'AdminPage':
+  //       return <AdminPage />
+  //     default:
+  //       return <Home />;
+  //   }
+  // };
 
   return (
     <ApolloProvider client={client}>
       <Router>
         <StoreProvider>
-          <Header currentPage={currentPage} handlePageChange={handlePageChange} />
-          <main >
-            {
-              // Render the component returned by 'renderPage()'
-              renderPage(currentPage) 
-            }
-          </main>
-          <Footer/>
+          <Header currentPage={currentPage} />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/AdminPage" component={AdminPage} />
+            <Route exact path="/Portfolio" component={Portfolio} />
+            <Route exact path="/Contact" component={Contact} />
+            <Route exact path="/About" component={About} />
+            <Route exact path="/Map" component={Map} />
+            <Route component={Home} />
+            <ProtectedRoute to="/admin_page" isAuthenticated={isAuthenticated} component={AdminAccess} />
+            <ProtectedRoute component={AdminAccess} />
+          </Switch>
+          <Footer />
         </StoreProvider>
       </Router>
     </ApolloProvider>
--- client/src/pages/AdminPage/index.js
+++ client/src/pages/AdminPage/index.js
@@ -1,14 +1,4 @@
-// MVP: page holds admin profile data, data collected from user contact form, and form for entering testimonial info
-// Nice to have: location to enter client list data, location to add photos and photo data
-// import Auth from '../../utils/auth';
-// import React, { useState } from 'react';
-// import { StoreProvider } from './utils/GlobalState';
-// import { useStoreContext } from "../../utils/GlobalState";
-
 import React from "react";
-// import { useStoreContext } from "../../utils/GlobalState";
-import Auth from "../../utils/auth";
-
 import MsgList from "../../components/MsgList";
 import TstList from "../../components/TstList";
 import OwnerInfo from "../../components/OwnerInfo";
@@ -17,7 +7,7 @@ import './adminmodal.css'
 const AdminPage = (props) => {
   return (
     <div>
-      {Auth.loggedIn() ? (
+      {props.isAuthenticated ? (
         <div className="pad">
           <MsgList />
           <TstList />
